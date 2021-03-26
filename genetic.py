d39bb77f9bb9b3721d28a05c867816f4fd8f589b
Rishi <rishi.jain13@nmims.edu.in>
2021-03-26 14:06:53 +0530

    Added genre-measurement

---
+++ genetic.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+# Constants
+INITIAL_POPULATION=50
+MUTATION_RATE=0.05
+NUM_OF_GENERATIONS=50
+FEATURE_LENGTH=21
+WINNER_PER_GEN=5
+
+
+def cost_function(actual_ratings, predicted_ratings):
+    """Cost function to optimize using genetic method."""
+
+
+
+def genetic_optimize(actual_ratings, predicted_ratings):
+    """Get genetic feature weights using crossover and mutation."""
